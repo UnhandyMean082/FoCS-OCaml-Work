fd3508e1ba78c387b9973d1291fb76f19b3270de
Aarav Agarwal <aarav02006@gmail.com>
2025-10-25 18:53:43 +0100

    Fixed logic error for creating years within tester program.

--- supervision1/local/representations/_build/default/bin/dates.ml
+++ supervision1/local/representations/_build/default/bin/dates.ml
@@ -5,5 +5,5 @@ let my_year: year = make 2025
 
 let () =
   Printf.printf "The current year is: %d\n" (get my_year);
-  Printf.printf "2049 Less than equal to 1950: %b\n1950 Less than equal to 2049: %b\n" (is_lteq (make 2049) (make 1950)) (is_lteq (make 1950) (make 2049));
+  Printf.printf "2049 Less than equal to 1950: %b\n1950 Less than equal to 2049: %b\n" (is_lteq (make 49) (make 50)) (is_lteq (make 50) (make 49));
   Printf.printf "Adding 5 years to 2025 gives: %d\n" (get (add my_year 5))
