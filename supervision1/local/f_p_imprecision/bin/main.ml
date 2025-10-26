eaab82e6f2e849777063b4636f4f1dd266d5cdda
Aarav Agarwal <aarav02006@gmail.com>
2025-10-26 19:22:08 +0000

    Implemented golden ratio formula to illustrate floating point imprecision.

--- supervision1/local/f_p_imprecision/bin/main.ml
+++ supervision1/local/f_p_imprecision/bin/main.ml
@@ -5,5 +5,15 @@ let mult (x: float) (n: int) =
   in
   loop x n 0.0
 
+let golden (n: int) =
+  let start = (1. +. sqrt 5.) /. 2.
+  in
+  let rec loop (n: int) (total: float) =
+    if n <= 0 then total
+    else loop (n - 1) (1. /. (total -. 1.))
+  in
+  loop n start
+
 let () =
-  Printf.printf "%f\n" (mult 0.1 1000000)
+  Printf.printf "%f\n" (mult 0.1 1000000);
+  Printf.printf "%f\n" (golden 50)
