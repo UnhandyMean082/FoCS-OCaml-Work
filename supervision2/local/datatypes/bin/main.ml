833b58ffb74678d400b32b13eeb8f624be03d639
Aarav Agarwal <aarav02006@gmail.com>
2025-11-10 04:11:39 +0000

    Implemented the arithmetic type for expressions.

--- supervision2/local/datatypes/bin/main.ml
+++ supervision2/local/datatypes/bin/main.ml
@@ -6,5 +6,12 @@ type 'a utree =
   | Lf
   | Br of 'a * 'a utree
 
+type arithmetic =
+  | Const of float
+  | Var of string
+  | Neg of arithmetic
+  | Add of arithmetic * arithmetic
+  | Mul of arithmetic * arithmetic
+
 let () =
   Printf.printf "Hello, world!\n"
